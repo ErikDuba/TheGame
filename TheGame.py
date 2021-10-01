aba43f267e7c91e26694bf04b72ce9d2597a64ce
ErikDuba <80678269+ErikDuba@users.noreply.github.com>
2021-10-01 23:12:25 +0200

    Update TheGame.py

--- TheGame.py
+++ TheGame.py
@@ -1,23 +1,28 @@
+import time
+import os
+def clear():
+    command = 'cls'
+    os.system(command)
+
+clear()
 print(' ___________________________________________________________________')
 print('| Welcome to the game:                                              |')
 print('| This game is about you who awakes with no memorie of the past     |')
 print('| You will encounter choices you have to make                       |')
-print('| Choose wisely because you might end up dead                       |')
+print('| Choose wisely because your life might depend on those choices     |')
 print('| Good luck...                                                      |')
 print('|___________________________________________________________________|')
 print('|                                                                   |')
 print('| You awake in total darkness, you light a candle to see that you   |')
 print('| are inside an arena.                                              |')
 print('| Suddenly a huge door opens and a dragon comes out.                |')
-print('| You see two potential weapons:                                    |')
+print('| You see three things laying on the ground:                        |')
 print('| Option 1- A comically large spoon                                 |')
 print('| Option 2- A backpack filled with paper airplanes strapped with    |')
 print('| bombs                                                             |')
 print(' ___________________________________________________________________')
 print('')
-print('You wake up in total darkness, you light a candle and find yourself in an arena. ')
-print('Suddenly a hatch opens and a huge dragon comes out. There are 2 potential weapons on the ground: ')
-keuze1 = input('a comically large spoon and backpack full of paper planes with bombs on them, which one do you take? spoon/backpack: ')
+keuze1 = input('You can only take one item, which one do you take? spoon/backpack: ')
 if keuze1 == 'spoon':
     print('')
     keuze2 = input('You grab the comically large spoon. You try to kill the dragon but soon notice that it\'s a tough dragon. \nYou see a wound on his wing and think that it is his weakspot, you also see a hole in the ground with a ladder going down. What do you do? weakspot/ladder: ')
@@ -39,6 +44,56 @@ if keuze1 == 'spoon':
                 elif keuze5 == 'lockpick':
                     print('')
                     print('You choose the lock pick set. You drive out of the cave again and come across a large door with a security guard. He asks: "Do you have the key for the door?". You answer no but show your lock pick set. The security guard thinks you were trying to break in and cuts your legs off and you bleed to death...RIP ')
+        elif keuze3 == 'blue':
+            print('')
+            keuze6 = input('You choose the blue door. The door closes behind you and you feel you are trapped by 2 nailbeds, then you are roasted alive and eaten by some dogs...RIP ')        
     elif keuze2 == 'ladder': 
         print('')
-        keuzeundefined = input('You take the ladder. It gets darker as you go down, suddenly the ladder stops and you fall down. You take out your candle and see that you have entered a cave with all sleeping dogs. You remember the saying \'don\'t wake sleeping dogs\'. You see a corridor with a weak lying at the end but also think: maybe one of these dogs can help me ')
+        print('You take the ladder. It gets darker as you go down, suddenly the ladder stops and you fall down. ')
+        print('You take out your candle and see that you have entered a cave filled with sleeping dogs. You remember the saying \'don\'t wake sleeping dogs\'.')
+        print('You see a corridor with a faint light at the end but also think: maybe one of these dogs can help me.')
+        print('You wake up a sleeping dog. He seems like a wise old friendly dog and asks if he knows what to do. The dog says: "Woof woef wöof woof wof wuf".')
+        print('You have no idea what he is saying but suddenly you see a smooth purple object in your hand, you recognize it as a dog language-translator and find out that he is saying:')
+        print('"You\'re not supposed to be here, follow me." You follow him until you come to an exit, you thank the dog and continue on your own. You walk out and see a helicopter on fire ')
+        keuze7 = input('Do you walk towards the flaming helicopter or do you walk past it? heli/walk past: ')
+        if keuze7 == 'heli' and subkeuze1 == 'yes':
+            print('')
+            print('As you walk closer and closer, the helicopter explodes. A pile of ashes lands before your feet, you remember the beaker that you picked up.')
+            print('You throw it on the pile of ash and a person that looks just like you appears')
+        elif keuze7 == 'heli':
+            print('Inception...RIP')
+elif keuze1 == 'backpack':
+    print('')
+    print('You take the backpack. You grab a plane, throw it at the dragon, the dragon and the plane have a huge dogfight. ')
+    print('The plane comes right behind the dragon and explodes ....... Suddenly the dragon comes flying out of the fire and you are friends. You can sit on its back and fly away.')
+    print('You land in Zimbabwe. There is water shortage there and you find out that the dragon can spew water instead of fire. You help a few villages and are faced with a choice: ')
+    keuze8 = input('Do I go back home by plane or do I keep helping people with the dragon? home/help: ')
+    if keuze8 == 'home':
+        print('')
+        print('You choose to go home with the airport.')
+        print('You never even arrive at the airport because in the taxi to the airport you were robbed, cut into pieces and your organs sold on the black market...RIP ')
+    elif keuze8 == 'help':
+        print('')
+        print('You choose to help solve the water shortage, you start with a few small villages but quickly move on to help large cities.')
+        keuze9 = input('The head of state Emmerson Mnangagwa asks you if you want to become emperor of zimbabwe, do you accept his offer? yes/no: ')
+        if keuze9 == 'no':
+            print('')
+            print('You decline his offer. On the way to Lesotho the dragon loses its other wing, you plummit towards earth from 12 kilometers high...RIP ')
+        elif keuze9 == 'yes':
+            print('')
+            print('You accept his offer and are now Emperor of Zimbabwe.')
+            print('17 years later you are still Emperor of Zimbabwe with the Dragon as an accomplice, you have 12 Nobel Prizes and the average income per household in Zimbabwe is now the highest in the world.')
+            print('You are happy with what you have achieved but want more, the plan: conquer the world.')
+            keuze10 = input('You think the dragon won\'t like the idea, so you\'re in a dilemma: do I continue on my own or with my dragon? with/without: ')
+            if keuze10 == 'without':
+                print('')
+                print('You go on on your own. You almost conquered the whole world when. You are sitting in a mansion somewhere off the coast of Mexico when suddenly the Dragon comes back and fills you house with water. You drown...RIP')
+            elif keuze10 == 'with':
+                print('')
+                print('You ask the dragon to help, the dragon says yes and you call in the entire Zimbabwean army. First you take over all of Africa, then Europe, Russia and Asia.')
+                print('When you come to the USA ')
+
+    #subkeuze1 = ('You see a beaker laying on the ground, do you take it? yes/no: ')
+    #if subkeuze1 == 'yes':
+
+
